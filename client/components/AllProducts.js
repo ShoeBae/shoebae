35c321cbf8c6211a49e2086779ad94a5a7165f90
Morgan Crouch <morganloel@gmail.com>
2019-01-08 12:09:48 -0600

    added class to all products

---
+++ client/components/AllProducts.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react'
+import Navbar from './Navbar'
+import {Link} from 'react-router-dom'
+import {connect} from 'react-redux'
+import {fetchProducts, deleteProduct} from '../store/products'
+
+class AllProducts extends Component {
+  componentDidMount() {
+    this.props.fetchProducts()
+  }
+  render() {
+    return <h1>Hello World!</h1>
+  }
+}
+
+///map to state
+//export connected state
