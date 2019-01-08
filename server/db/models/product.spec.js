0933613e57fd77cae779d5784feb1c808be5dc76
Evan Alto <evanalto@Evans-MacBook.local>
2019-01-08 10:51:45 -0600

    install nyc package, and added it to test script

---
+++ server/db/models/product.spec.js
@@ -0,0 +1,9 @@
+const {expect} = require('chai')
+const db = require('../index')
+const Product = db.model('product')
+
+describe('Product model', () => {
+  beforeEach(() => {
+    return db.sync({force: true})
+  })
+})
