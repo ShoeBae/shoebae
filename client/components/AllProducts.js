85b29c136fcb5bfcd4d1bd74f4176f8d1feca770
Jingtin Tim Lin <lin.jingt@gmail.com>
2019-01-09 14:05:53 -0600

    figuring out views

--- client/components/AllProducts.js
+++ client/components/AllProducts.js
@@ -13,41 +13,38 @@ class AllProducts extends Component {
   }
 
   componentDidMount() {
-    console.log('TESTING', this.props.products)
     this.props.fetchProducts()
+    this.setState({currentView: this.props.products})
   }
+
   handleChange(event) {
-    console.log(event.target.value)
+    this.setState({currentView: event.target.value})
   }
 
   render() {
-    console.log(this.props)
-    return (
-      <div className="productsList">
-        <form>
-          SORT{' '}
-          <select
-            name="sortBy"
-            onChange={event => {
-              console.log('lets sort this real soon!')
-            }}
-          >
-            <option value="brand">Brand</option>
-            <option value="model">Name</option>
-            <option value="price">Price</option>
-          </select>
-        </form>
-        {this.props.products.map(product => (
-          <div key={product.id}>
-            <Link className="linkText" to={`/products/${product.id}`}>
-              <img src={product.imageUrl} /> {product.model}
-              <br />
-              {'$' + product.price}
-            </Link>
-          </div>
-        ))}
-      </div>
-    )
+    let currentProducts = [...this.props.products]
+    if (products)
+      return (
+        <div className="productsList">
+          <form>
+            SORT{' '}
+            <select name="sortBy" onChange={this.handleChange}>
+              <option value="brand">Brand</option>
+              <option value="model">Name</option>
+              <option value="price">Price</option>
+            </select>
+          </form>
+          {this.props.products.map(product => (
+            <div key={product.id}>
+              <Link className="linkText" to={`/products/${product.id}`}>
+                <img src={product.imageUrl} /> {product.model}
+                <br />
+                {'$' + product.price}
+              </Link>
+            </div>
+          ))}
+        </div>
+      )
   }
 }
 
