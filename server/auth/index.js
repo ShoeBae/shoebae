50249bc717e42d031865db5ce20fd0315041055f
Morgan Crouch <morganloel@gmail.com>
2019-01-10 17:06:15 -0600

    added needed sessions

--- server/auth/index.js
+++ server/auth/index.js
@@ -7,6 +7,7 @@ module.exports = router
 router.post('/login', async (req, res, next) => {
   try {
     const user = await User.findOne({where: {email: req.body.email}})
+
     if (!user) {
       console.log('No such user found:', req.body.email)
       res.status(401).send('Wrong username and/or password')
@@ -14,9 +15,6 @@ router.post('/login', async (req, res, next) => {
       console.log('Incorrect password for user:', req.body.email)
       res.status(401).send('Wrong username and/or password')
     } else {
-      const cart = await Cart.findOne({where: {userId: user.id}})
-      //refract code below
-      console.log('THIS IS A CART', cart)
       req.login(user, err => (err ? next(err) : res.json(user)))
     }
   } catch (err) {
