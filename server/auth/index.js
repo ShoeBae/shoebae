91fac85692ecd72fe55aefb83193e8e0de46454c
Morgan Crouch <morganloel@gmail.com>
2019-01-10 15:31:10 -0600

    added sessions to sign up but not login

--- server/auth/index.js
+++ server/auth/index.js
@@ -14,7 +14,14 @@ router.post('/login', async (req, res, next) => {
       console.log('Incorrect password for user:', req.body.email)
       res.status(401).send('Wrong username and/or password')
     } else {
-      Cart.find({where: {userId: user.id, sessionId: req.session}})
+      const cart = await Cart.findOne({where: {userId: user.id}})
+      //refract code below
+      console.log('THIS IS A CART', cart)
+      if (cart) {
+        await Cart.update({sessionId: req.sessionID})
+      }
+      ///ERROR ABOVE, TRYING TO UPDATE EXISTING CART
+
       req.login(user, err => (err ? next(err) : res.json(user)))
     }
   } catch (err) {
@@ -25,10 +32,16 @@ router.post('/login', async (req, res, next) => {
 router.post('/signup', async (req, res, next) => {
   try {
     const user = await User.create(req.body)
-    Cart.find({where: {sessionId: req.session}}).then(
-      Cart.update({userID: user.id})
-    )
-    await Cart.create({where: {userId: user.id, sessionId: req.session}})
+    //add lodash above
+    const cart = await Cart.find({where: {sessionId: req.sessionID}})
+    //refract code below
+    console.log('THIS IS A CART', cart)
+    if (cart) {
+      await Cart.update({where: {userID: user.id}})
+    } else {
+      await Cart.create({userId: user.id, sessionId: req.sessionID})
+    }
+
     req.login(user, err => (err ? next(err) : res.json(user)))
   } catch (err) {
     if (err.name === 'SequelizeUniqueConstraintError') {
