d35b2cbf1398f8bcf2c6561d48781e4b4f060aa3
Jingtin Tim Lin <lin.jingt@gmail.com>
2019-01-10 18:58:01 -0600

    moved client id and secret to secrets.js

--- server/auth/google.js
+++ server/auth/google.js
@@ -4,21 +4,6 @@ const GoogleStrategy = require('passport-google-oauth').OAuth2Strategy
 const {User} = require('../db/models')
 module.exports = router
 
-/**
- * For OAuth keys and other secrets, your Node process will search
- * process.env to find environment variables. On your production server,
- * you will be able to set these environment variables with the appropriate
- * values. In development, a good practice is to keep a separate file with
- * these secrets that you only share with your team - it should NOT be tracked
- * by git! In this case, you may use a file called `secrets.js`, which will
- * set these environment variables like so:
- *
- */
-process.env.GOOGLE_CLIENT_ID =
-  '952531598775-j5lhnhob7q28pfqb970edkjju7gmb158.apps.googleusercontent.com'
-process.env.GOOGLE_CLIENT_SECRET = 'ddCzqD3oJDusXKG1OYHXFsVG'
-process.env.GOOGLE_CALLBACK = 'http://localhost:8080/auth/google/callback'
-
 if (!process.env.GOOGLE_CLIENT_ID || !process.env.GOOGLE_CLIENT_SECRET) {
   console.log('Google client ID / secret not found. Skipping Google OAuth.')
 } else {
