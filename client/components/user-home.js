a8ca290d7a0269d0aa974d691504c46175ed4a0e
Morgan Crouch <morganloel@gmail.com>
2019-01-11 12:39:34 -0600

    made userhome into class function

--- client/components/user-home.js
+++ client/components/user-home.js
@@ -1,31 +1,52 @@
-import React from 'react'
+import React, {Component} from 'react'
 import PropTypes from 'prop-types'
 import {connect} from 'react-redux'
 
 /**
  * COMPONENT
  */
-export const UserHome = props => {
-  const {email, userId} = props
-  console.log('USER IS ADMIN?', props.userInfo.isAdmin)
+class UserHome extends Component {
+  constructor(props) {
+    super()
+  }
 
-  return (
-    <div>
-      <h3>Welcome, {email}</h3>
-      <h2>ORDER HISTORY</h2>
-      <a>Account Settings - MAKE THIS ACTIVELY SHOW DETAILS ON CLICK</a>
-      <div>{props.userInfo.email}</div>
-      <div>{props.userInfo.email}</div>
-      <button type="button">update account</button>
-      {props.userInfo.isAdmin ? (
-        <div>ADD ADMIN INFO HERE</div>
-      ) : (
-        <div>
-          <div>NO ADMIN ACCESS TEST</div>
-        </div>
-      )}
-    </div>
-  )
+  render() {
+    const {email, userId} = this.props
+
+    return (
+      <div>
+        <h3>Welcome, {email}</h3>
+
+        {this.props.userInfo.isAdmin ? (
+          <React.Fragment>
+            <div>ADD ADMIN INFO HERE</div>
+            <br />
+
+            <h4>ACTIVE CARTS</h4>
+            <ul href="# ">SHOEBAE CLIENTS </ul>
+            <ul href="#">GUESTS</ul>
+            <br />
+            <h4>ORDER HISTORY</h4>
+            <ul href="# ">SHOEBAE CLIENTS </ul>
+            <ul href="#">GUESTS</ul>
+          </React.Fragment>
+        ) : (
+          <div>
+            <React.Fragment>
+              <h4>ACCOUNT DETAILS</h4>
+              <ul href="# ">{email}</ul>
+              <ul href="#">password</ul>
+              <button type="button">update account</button>
+              <br />
+              <h4>ORDER HISTORY</h4>
+              <ul href="# ">map over client history </ul>
+            </React.Fragment>
+            <div>NO ADMIN ACCESS TEST</div>
+          </div>
+        )}
+      </div>
+    )
+  }
 }
 
 /**
